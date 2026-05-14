3f9c789c50a170393f68c9edf6d683ea3812bad3
Mario Perez <mariopc.ingeniero@gmail.com>
2026-05-14 16:29:51 -0400

    Agregar base oficial de datos ERP

--- public/chunk-IBJRAVY2.js
+++
@@ -1,1 +0,0 @@
-var t={production:!1,apiUrl:"http://localhost:3000/api",baseUrl:"http://localhost:3000"};export{t as a};
---
+++ public/chunk-NWFPUQS4.js
@@ -0,0 +1,1 @@
+var t={production:!1,apiUrl:"http://192.168.100.165:3000/api",baseUrl:"http://192.168.100.165:3000"};export{t as a};
